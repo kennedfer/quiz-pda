d3129a2fa37eda1535cf79ae610ce3e46936a01b
= <=>
2024-02-12 22:51:14 +0000

    feat: Adiantando grande parte da logica do jogo, ja eh possivel avancar de pergunta

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,97 @@
-function loadData() {
-    // const quizData = fetch('server route');
+class QuizGame {
+  _questions;
+  _currentQuestion = 0;
 
-    const quizData = [
-        {
-            author: "kennedfer",
-            questions:[
-                {
-                    question: "qual a melhor linguagem de programacao? 1",
-                    answers:[
-                        "javascript", "java", "python", "php"
-                    ],
-                    correct_answer: "java"
-                }
-            ]
-        },
-        {
-            author: "kennedfer",
-            questions:[
-                {
-                    question: "qual a melhor linguagem de programacao? 2",
-                    answers:[
-                        "javascript", "java", "python", "php"
-                    ],
-                    correct_answer: "java"
-                }
-            ]
-        },
-        {
-            author: "kennedfer",
-            questions:[
-                {
-                    question: "qual a melhor linguagem de programacao? 3",
-                    answers:[
-                        "javascript", "java", "python", "php"
-                    ],
-                    correct_answer: "java"
-                }
-            ]
-        },
-        {
-            author: "kennedfer",
-            questions:[
-                {
-                    question: "qual a melhor linguagem de programacao? 4",
-                    answers:[
-                        "javascript", "java", "python", "php"
-                    ],
-                    correct_answer: "java"
-                }
-            ]
-        },
-    ]
+  _questionElement;
+  _questionIndicatorElement;
 
-    return quizData;
+  constructor(questions) {
+    this._questions = questions;
+
+    this._questionElement = document.getElementById("question");
+    this._questionIndicatorElement =
+      document.getElementById("question-indicator");
+  }
+
+  _paintQuestion() {
+    this._questionElement.textContent =
+      this._questions[this._currentQuestion].question;
+
+    // A adicao do mais 1 garante que nao tenhamos 'Questao 0 de 4'
+    this._questionIndicatorElement.textContent = `Questao ${
+      this._currentQuestion + 1
+    } de ${this._questions.length}`;
+  }
+
+  setCurrentQuestion(value) {
+    this._currentQuestion = value;
+  }
+
+  _startInQuestion(currentQuestion) {
+    this.setCurrentQuestion(currentQuestion);
+    this._paintQuestion();
+  }
+
+  startGame() {
+    this._startInQuestion(0);
+  }
+
+  nextQuestion() {
+    this._startInQuestion(this._currentQuestion + 1);
+  }
+
+  checkAnswer(answer) {
+    const correctAnswer = this._questions[this._currentQuestion].correct_answer;
+    if (answer !== correctAnswer) {
+      return this._looseGame();
+    }
+
+    this.nextQuestion();
+  }
+
+  _looseGame() {
+    alert("Perdeu");
+  }
 }
 
-function checkAnswer(event){
-    alert(event);
+function loadData() {
+  // const quizData = fetch('server route');
+
+  const quizData = {
+    author: "kennedfer",
+    questions: [
+      {
+        question: "qual a melhor linguagem de programacao? 1",
+        answers: ["javascript", "java", "python", "php"],
+        correct_answer: "java",
+      },
+      {
+        question: "qual a melhor linguagem de programacao? 2",
+        answers: ["javascript", "java", "python", "php"],
+        correct_answer: "java",
+      },
+      {
+        question: "qual a melhor linguagem de programacao? 3",
+        answers: ["javascript", "java", "python", "php"],
+        correct_answer: "java",
+      },
+      {
+        question: "qual a melhor linguagem de programacao? 4",
+        answers: ["javascript", "java", "python", "php"],
+        correct_answer: "java",
+      },
+    ],
+  };
+
+  return quizData;
+}
+
+function checkAnswer(event) {
+  // O html adiciona espacos ao texto,o trim os remove
+  const currentAnswer = event.textContent.trim();
+  quizGame.checkAnswer(currentAnswer);
 }
 
 const quizData = loadData();
-
+const quizGame = new QuizGame(quizData.questions);
+quizGame.startGame();
